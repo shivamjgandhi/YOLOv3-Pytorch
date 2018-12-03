7909bd0df808ca9f62c34c881956302932a0c41d
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2018-12-03 13:01:20 -0500

    Started part 2, creating a Darknet class

--- darknet.py
+++ darknet.py
@@ -15,6 +15,54 @@ class DetectionLayer(nn.Module):
 		super(DetectionLayer, self).__init__()
 		self.anchors = anchors
 
+class Darknet(nn.Module):
+	def __init__(self, cfgfile):
+		super(Darknet, self).__init__()
+		self.blocks = parse_cfg(cfgfile)
+		self.net_info, self.module_list = create_modules(self.blocks)
+
+	"""
+
+	The forward pass of the network is implemented by overriding the
+	forward method of the nn.Module class. forward serves two purposes.
+	First, to calculate the output, and second, to transform the
+	output detection feature maps in a way that it can be processed
+	easier. 
+
+	"""
+
+	def forward(self, x, CUDA):
+		modules = self.blocks[1:]
+		output = {} # We cache the outputs for the route layer
+		write = 0
+		for i, module in enumerate(modules):
+			module_type = (module["type"])
+
+			if module_type == "convolutional" or module_type == "upsample":
+				x = self.module_list[i](x)
+
+			elif module_type == "route":
+				layers = module["layers"]
+				layers = [int(a) for a in layers]
+
+				if (layers[0] > 0):
+					layers[0] = layers[0] - i 
+
+				if len(layers) == 1:
+					x = outputs[i + layers[0]]
+
+				else:
+					if (layers[1] > 0):
+						layers[1] = layers[1] - i 
+
+					map1 = outputs[i + layers[0]]
+					map2 = outputs[i + layers[1]]
+
+					x = torch.cat((map1, map2), 1)
+			elif module_type == "shortcut":
+				from_ = int(module["from"])
+				x = outputs[i-1] + outputs[i+from_]
+
 def parse_cfg(cfgfile):
 	"""
 
