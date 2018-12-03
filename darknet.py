4635a3580f43ff7adb73a7863deccdc4686ea05f
Sjgandhi1998 <shivam.jgandhi@gmail.com>
2018-12-03 12:41:48 -0500

    Finished part 1

--- darknet.py
+++ darknet.py
@@ -63,111 +63,123 @@ def parse_cfg(cfgfile):
 	# We need to create our own modules for the rest of the layers
 	# by extending the nn.Module class
 
-	def create_modules(blocks):
-		net_info = blocks[0]
-		# nn.ModuleList is a class almost like a normal list containing
-		# nn.Module objects. 
-		module_list = nn.ModuleList()
-		# Need to keep track of number of filters in the layer on which 
-		# the convolutional layer is being applied. Here, 3 RGB channels
-		# therefore prev_filters = 3
-		prev_filters = 3
-		output_filters = []
+def create_modules(blocks):
+	"""
 
-		# Want to iterate over the list of blocks and create a module for
-		# each block as we go
-		for index, x in enumerate(blocks[1:]):
-			module = nn.Sequential()
+	Lessons learned:
+	- You can define classes before functions in this manner and still
+	create an executable script
+	- You can do "for index, x" loops to access both the index and val
+	- You can do "leaky_{}".format(index) to tell what the {} should be
 
-			# check the type of block
-			# create a new module for the block
-			# append to module_list
+	"""
+	net_info = blocks[0]
+	# nn.ModuleList is a class almost like a normal list containing
+	# nn.Module objects. 
+	module_list = nn.ModuleList()
+	# Need to keep track of number of filters in the layer on which 
+	# the convolutional layer is being applied. Here, 3 RGB channels
+	# therefore prev_filters = 3
+	prev_filters = 3
+	output_filters = []
 
-			if (x["type"] == "convolutional"):
-				# Get the info about the layer
-				activation = x["activation"]
-				try:
-					batch_normalize = int(x["batch_normalize"])
-					bias = False
-				except:
-					batch_normalize = 0
-					bias = True
+	# Want to iterate over the list of blocks and create a module for
+	# each block as we go
+	for index, x in enumerate(blocks[1:]):
+		module = nn.Sequential()
 
-				filters = int(x["filters"])
-				padding = int(x["pad"])
-				kernel_size = int(x["size"])
-				stride = int(x["stride"])
+		# check the type of block
+		# create a new module for the block
+		# append to module_list
 
-				if padding:
-					pad = (kernel_size - 1) // 2
-				else:
-					pad = 0
+		if (x["type"] == "convolutional"):
+			# Get the info about the layer
+			activation = x["activation"]
+			try:
+				batch_normalize = int(x["batch_normalize"])
+				bias = False
+			except:
+				batch_normalize = 0
+				bias = True
 
-				# Add the convolutional layer
-				conv = nn.Conv2D(prev_filters, filters, kernel_size, stride, pad, bias=bias)
-				module.add_module("conv_{0}".format(index), conv)
+			filters = int(x["filters"])
+			padding = int(x["pad"])
+			kernel_size = int(x["size"])
+			stride = int(x["stride"])
 
-				# Add the batch norm layer
-				if batch_normalize:
-					bn = nn.BatchNorm2d(filters)
-					module.add_module("batch_norm_{0}".format(index), bn)
+			if padding:
+				pad = (kernel_size - 1) // 2
+			else:
+				pad = 0
 
-				# Check the activation
-				# It is either linear or a leaky ReLu for YOLO
-				if activation == "leaky":
-					activn = nn.LeakyReLU(0.1, inplace=True)
-					module.add_module("leaky_{0}".format(index), bn)
+			# Add the convolutional layer
+			conv = nn.Conv2d(prev_filters, filters, kernel_size, stride, pad, bias=bias)
+			module.add_module("conv_{0}".format(index), conv)
 
-			# If it's an upsampling layer
-			# We use bilinear2dUpsampling
-			elif (x["type"] == "upsample"):
-				stride = int(x["stride"])
-				upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
-				module.add_module("upsample_{}".format(index), upsample)
+			# Add the batch norm layer
+			if batch_normalize:
+				bn = nn.BatchNorm2d(filters)
+				module.add_module("batch_norm_{0}".format(index), bn)
 
-			# If it's a route layer
-			elif (x["type"] == "route"):
-				x["layers"] = x["layers"].split(',')
-				# Start of a route
-				start = int(x["layers"][0])
-				# end, if there exists one
-				try:
-					end = int(x["layers"][1])
-				except:
-					end = 0
-				# Positive annotation
-				if start > 0:
-					start = start - index
-				if end > 0:
-					end = end - index
+			# Check the activation
+			# It is either linear or a leaky ReLu for YOLO
+			if activation == "leaky":
+				activn = nn.LeakyReLU(0.1, inplace=True)
+				module.add_module("leaky_{0}".format(index), bn)
 
-				route = EmptyLayer()
-				module.add_module("route_{0}".format(index), route)
-				if end < 0:
-					filters = output_filters[index + start] + output_filters[index + end]
-				else:
-					filters = output_filters[index + start]
+		# If it's an upsampling layer
+		# We use bilinear2dUpsampling
+		elif (x["type"] == "upsample"):
+			stride = int(x["stride"])
+			upsample = nn.Upsample(scale_factor = 2, mode = "bilinear")
+			module.add_module("upsample_{}".format(index), upsample)
 
-			# shortcut corresponds to skip connection
-			elif x["type"] == "shortcut":
-				shortcut = EmptyLayer()
-				module.add_module("shortcut_{}".format(index), shortcut)
+		# If it's a route layer
+		elif (x["type"] == "route"):
+			x["layers"] = x["layers"].split(',')
+			# Start of a route
+			start = int(x["layers"][0])
+			# end, if there exists one
+			try:
+				end = int(x["layers"][1])
+			except:
+				end = 0
+			# Positive annotation
+			if start > 0:
+				start = start - index
+			if end > 0:
+				end = end - index
 
-			# Yolo is the detection layer
-			elif x["type"] == "yolo":
-				mask = x["mask"].split(",")
-				mask = [int(x) for x in mask]
+			route = EmptyLayer()
+			module.add_module("route_{0}".format(index), route)
+			if end < 0:
+				filters = output_filters[index + start] + output_filters[index + end]
+			else:
+				filters = output_filters[index + start]
 
-				anchors = x["anchors"].split(",")
-				anchors = [int(a) for a in anchors]
-				anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
-				anchors = [anchors[i] for i in mask]
+		# shortcut corresponds to skip connection
+		elif x["type"] == "shortcut":
+			shortcut = EmptyLayer()
+			module.add_module("shortcut_{}".format(index), shortcut)
 
-				detection = DetectionLayer(anchors)
-				module.add_module("Detection_{}".format(index), detection)
+		# Yolo is the detection layer
+		elif x["type"] == "yolo":
+			mask = x["mask"].split(",")
+			mask = [int(x) for x in mask]
 
-			module_list.append(module)
-			prev_filters = filters
-			output_filters.append(filters)
+			anchors = x["anchors"].split(",")
+			anchors = [int(a) for a in anchors]
+			anchors = [(anchors[i], anchors[i+1]) for i in range(0, len(anchors),2)]
+			anchors = [anchors[i] for i in mask]
 
-		return
+			detection = DetectionLayer(anchors)
+			module.add_module("Detection_{}".format(index), detection)
+
+		module_list.append(module)
+		prev_filters = filters
+		output_filters.append(filters)
+
+	return (net_info, module_list)
+
+blocks = parse_cfg("cfg/yolov3.cfg")
+print(create_modules(blocks))
